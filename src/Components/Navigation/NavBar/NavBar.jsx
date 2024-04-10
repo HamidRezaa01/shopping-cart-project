441a7ce0b3ab6c3c0db0f0a66c09566d902d6e7a
HamidReza <mazinanihamidreza80@gmail.com>
2024-04-10 11:23:19 +0330

    added navbar component

---
+++ src/Components/Navigation/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const NavBar = () => {
+
+    return (
+        
+
+    )
+}
+
+export default NavBar
