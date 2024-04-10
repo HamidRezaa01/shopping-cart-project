a521ea2ac53eb09286d3a3148d838e0d319d1690
HamidReza <mazinanihamidreza80@gmail.com>
2024-04-10 14:18:57 +0330

    responsive menu done

--- src/Components/Navigation/NavBar/NavBar.jsx
+++ src/Components/Navigation/NavBar/NavBar.jsx
@@ -12,6 +12,9 @@ const NavBar = () => {
         { id: 4, navItem: 'About' },
     ]
 
+    const handlerNav = () => {
+        setNav(!nav)
+    }
 
 
     return (
@@ -24,17 +27,30 @@ const NavBar = () => {
                 ))}
             </ul>
 
-            <span className='flex hidden md:flex border justify-center items-center'>
-                <BsCartPlus className='text-white'/>
+            <span className='bg-green-900 flex hidden md:flex px-4 rounded justify-center items-center'>
+                <BsCartPlus className='text-white' />
                 <a className='p-2 text-white inline' href="#">Shopping</a>
             </span>
 
-            
+            <section onClick={handlerNav} className='block md:hidden text-white'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </section>
 
-
-
-
-
+            <ul className={
+                nav ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-blue-950 ease-in-out duration-500'
+                    : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+            }>
+                <section>
+                    <h3 className='text-white w-full text-3xl font-bold p-2'>LOGO</h3>
+                    {navItems.map((item) => (
+                        <li className=' p-4 border-b rounded-xl duration-300 text-white' key={item.id}>{item.navItem}</li>
+                    ))}
+                    <span className='w-full flex bg-green-900 curser-pointer justify-center p-4 items-center'>
+                        <BsCartPlus className='text-white' />
+                        <a className='text-white inline ml-2' href="#">Shopping</a>
+                    </span>
+                </section>
+            </ul>
         </section>
     )
 }
