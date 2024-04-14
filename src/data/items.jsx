91b6a8577958ddcfa5ec4bb3f75f806058273ba6
HamidReza <mazinanihamidreza80@gmail.com>
2024-04-14 10:19:06 +0330

    added data

--- src/data/items.jsx
+++ src/data/items.jsx
@@ -1,8 +1,8 @@
 
 const productList = [
-    {id: 2, title: 'Green Leafed Plant', price: 99.74, img: '/images/product1.webp'},
-    {id: 3, title: 'Green Succulent', price: 98.94, img: '/images/product2.webp'},
-    {id: 4, title: 'Swiss Cheese Plant', price: 99.74, img: '/images/product3.png'},
+    {id: 2, title: 'Green Leafed Plant', price: 99.74, img: '/images/product.png'},
+    {id: 3, title: 'Green Succulent', price: 98.94, img: '/images/product.png'},
+    {id: 4, title: 'Swiss Cheese Plant', price: 99.74, img: '/images/product.png'},
 ]
 
 const getProductData = (productID) => {
@@ -11,4 +11,4 @@ const getProductData = (productID) => {
     return product
 }
 
-export default {productList, getProductData}
+export { productList, getProductData }
