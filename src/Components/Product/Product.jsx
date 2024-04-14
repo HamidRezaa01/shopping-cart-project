6a426695723ccb99237c06f3f44c6c1950a1d9e8
HamidReza <mazinanihamidreza80@gmail.com>
2024-04-14 10:21:38 +0330

    added product style

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -3,15 +3,15 @@
 const Product = ({ product }) => {
     return ( 
         <>
-          <section className='flex justify-between items-center bg-green-950 w-96 h-64 mt-10 mb-20 p-2'>
+          <section className='flex justify-between rounded items-center bg-green-950 w-96 h-64 mt-14 mb-20 p-2'>
             <section>
                 <img src={product.img} alt="" className="w-auto"/>
             </section>
             <section className="leading-10">
-                <h2 className="text-xl">{product.title}</h2>
-                <p className="text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, nulla!</p>
+                <h2 className="text-xl mb-2">{product.title}</h2>
+                <p className="text-base mb-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, nulla!</p>
                 <p>${product.price}</p>
-                <button className='bg-slate-900 px-4'>Add to cart</button>
+                <button className='bg-slate-900 px-4 rounded'>Add to cart</button>
             </section>
           </section>
         </>
