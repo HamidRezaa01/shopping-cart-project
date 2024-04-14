d6e61c282bd3f95fb24944209d21082a4dfae40b
HamidReza <mazinanihamidreza80@gmail.com>
2024-04-14 10:22:00 +0330

    import cart provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import NavBar from "./Components/Navigation/NavBar/NavBar"
 import Shop from './pages/Shop'
+import { CartProvider } from '../src/context/CartContext'
 
 const App = () => {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Shop />} />
-      </Routes>
+      <CartProvider>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+        </Routes>
+      </CartProvider>
     </>
   )
 }
